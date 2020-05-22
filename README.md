5c8517f320f73b29f3a988d9e5a78d16e57ca1ce
Shubham Goel <37694398+shubhamgoel90@users.noreply.github.com>
2020-05-22 20:11:52 +0530

    Covid Doctor

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-"# Covid-Doctor" 
+# Covid Doctor
+
+This project is an automated covid-19 doctor helping in identifying the cases via
+
+- Pulse Scanning
+- Xray Scanning
+
+### Motivation
+
+In these difficult times everybody has to be causious for which they need to have a self assesment tool which they can use for scanning themselves anytime
+
+### Screenshot
+
+![picture alt](static/images/1.png "Title is optional")
+![picture alt](static/images/2.png "Title is optional")
+![picture alt](static/images/3.png "Title is optional")
+![picture alt](static/images/4.png "Title is optional")
+![picture alt](static/images/5.png "Title is optional")
+
+### Tech Stack
+
+- Open CV for image detection
+- Keras for deep learning
+- Flask for web deployment
+- Machine learning for pulse scanning models
+
+### Services Offered By Covid Doctor
+
+- Pulse Scanning
+- Xray Scanning
+- Real time COVID Updates
+- Chatbot for real time assistance
--- app.py
+++ app.py
@@ -1,9 +1,6 @@
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask,request,redirect,url_for,render_template
 import camera
-import sys
-import os
-import glob
-import re
+import sys,os,glob,re
 import numpy as np
 import requests
 import json
@@ -11,42 +8,40 @@ import feat
 from keras.preprocessing import image
 import cv2
 from keras.models import load_model
-from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.applications.imagenet_utils import preprocess_input,decode_predictions
 from werkzeug.utils import secure_filename
 from keras.models import model_from_json
 import keras
 from keras.layers import *
-from keras.models import Model, load_model
+from keras.models import Model , load_model
 from keras.preprocessing import image
 from keras.utils import np_utils
 from keras.models import model_from_json
 from keras.applications.resnet50 import ResNet50
 from keras.optimizers import Adam
 import matplotlib.pyplot as plt
-app = Flask(__name__, static_url_path="", static_folder="templates")
+app = Flask(__name__,static_url_path="",static_folder="templates")
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    data = requests.get(
-        "https://disease.sh/v2/countries/India?yesterday=true&strict=true")
+    data = requests.get("https://disease.sh/v2/countries/India?yesterday=true&strict=true")
     data_dict = data.json()
     var = "hello"
-    return render_template('index.html', data=data_dict)
-
+    return render_template('index.html',data = data_dict)
 
 @app.route('/detect')
 def detect():
     os.system('python get_pulse.py')
     return render_template('index.html')
 
-
 @app.route('/xray')
 def xray():
     os.system('python X_Ray_Detection.py')
     return render_template('index.html')
+
 
 
 @app.route('/analyze', methods=["POST"])
@@ -55,11 +50,9 @@ def analyze():
     if request.method == 'POST':
         f = request.files['file']
         basepath = os.path.dirname('uploads')
-        file_path = os.path.join(
-            basepath, 'uploads', secure_filename(f.filename))
+        file_path = os.path.join(basepath, 'uploads', secure_filename(f.filename))
         print("In the analyze funciton")
         f.save(file_path)
-        print("going into model predict")
 
         # pred_class = decode_predictions(preds, top=1)
         # result = str(pred_class[0][0][1])
